883e98c720250cdf2e1ef45dad9b82cc6f2ebd9f
Donizzy <108010549+Techiedevjs@users.noreply.github.com>
2023-09-01 11:47:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ const pages = [
 pages.map((p) => {
     const {tab, name, page} = p
     tab.addEventListener('click', () => {
+        if(name === 'home'){
+            document.querySelector('.popular').classList.add('hide-element')
+            document.querySelector('.homepage').classList.remove('hide-element')
+        }
         tab.classList.add('pressed');
         page.classList.remove('hide-element');
         pages.map((i) => {
@@ -44,46 +48,107 @@ pages.map((p) => {
         })
     })
 })
-let favouriteJobs = [
+const showPopularJobs = () => {
+    document.querySelector('.popular').classList.remove('hide-element')
+    document.querySelector('.homepage').classList.add('hide-element')
+}
+let jobs = [
     {
+        jobid: 1,
         company: "labdrill",
         position: "frontend developer",
-        detail: "Assist in developing user-facing feautures using HTML, CSS and JavaScript ",
+        detail: "Assist in developing user-facing features using HTML, CSS and JavaScript ",
         difficulty: 3,
         imageUrl: "images/job1.png",
-        pay: 540
+        pay: 540,
+        favorite: true,
+        popular: true
     },
     {
+        jobid: 2,
         company: "rangreen",
         position: "digital marketing specialist",
         detail: "selling our products in the digital space",
         difficulty: 2,
         imageUrl: "images/job3.png",
-        pay: 650
+        pay: 650,
+        favorite: false,
+        popular: true
     },
     {
+        jobid: 3,
         company: "namcorp",
         position: "audio creative lead",
         detail: "we just need our communication to be better, that's where you come in..",
         difficulty: 5,
         imageUrl: "images/job5.png",
-        pay: 595
+        pay: 595,
+        favorite: true,
+        popular: true
     },
     {
+        jobid: 4,
         company: "helix",
         position: "video content creator",
         detail: "well the goal is to create eye catching visual presentations",
         difficulty: 1,
         imageUrl: "images/job2.png",
-        pay: 1360
+        pay: 1360,
+        favorite: true
+    },
+    {
+        jobid: 5,
+        company: "Ragnorxk",
+        position: "digital marketing specialist",
+        detail: "selling our products in the digital space",
+        difficulty: 5,
+        imageUrl: "images/job3.png",
+        pay: 1650,
+        favorite: false
+    },
+    {
+        jobid: 6,
+        company: "robocorp",
+        position: "audio creative lead",
+        detail: "we just need our communication to be better, that's where you come in..",
+        difficulty: 4,
+        imageUrl: "images/job5.png",
+        pay: 395,
+        favorite: false
     },
 ]
-
+const pushAllJobs = (data) => {
+    document.querySelector('.alljobs').innerHTML = "";
+    data.map((job) => {
+        const {company, position, detail, difficulty, imageUrl, pay} = job;
+        document.querySelector('.alljobs').innerHTML += `
+        <article class="job">
+            <div class="job-details">
+                <img src="${imageUrl}" alt="">
+                <section>
+                    <h4>${company} - ${position}</h4>
+                    <p>${detail}</p>
+                    <div class="difficulty">
+                        Difficulty
+                        <div class="stars"></div>
+                    </div>
+                </section>
+            </div>
+            <div class="last">
+                <p>$${pay}</p>
+                <button>DETAILS</button>
+            </div>
+        </article>
+        `
+    })
+}
+pushAllJobs(jobs);
 const pushFavoriteJobs = (data) => {
     document.querySelector('.favorite-jobs').innerHTML = "";
     data.map((job) => {
-        const {company, position, detail, difficulty, imageUrl, pay} = job;
-        document.querySelector('.favorite-jobs').innerHTML += `
+        if(job.favorite){
+            const {company, position, detail, difficulty, imageUrl, pay} = job;
+            document.querySelector('.favorite-jobs').innerHTML += `
             <article class="job">
                 <div class="job-details">
                     <img src="${imageUrl}" alt="">
@@ -96,10 +161,41 @@ const pushFavoriteJobs = (data) => {
                         </div>
                     </section>
                 </div>
-                <p class="pay">$${pay}</p>
+                <div class="last">
+                    <p>$${pay}</p>
+                    <button>DETAILS</button>
+                </div>
             </article>
-        `
+            `
+        }
     })
 }
-
-pushFavoriteJobs(favouriteJobs);
+pushFavoriteJobs(jobs);
+const pushPopularJobs = (data) => {
+    document.querySelector('.popular-jobs').innerHTML = "";
+    data.map((job) => {
+        if(job.popular){
+            const {company, position, detail, difficulty, imageUrl, pay} = job;
+            document.querySelector('.popular-jobs').innerHTML += `
+            <article class="job">
+                <div class="job-details">
+                    <img src="${imageUrl}" alt="">
+                    <section>
+                        <h4>${company} - ${position}</h4>
+                        <p>${detail}</p>
+                        <div class="difficulty">
+                            Difficulty
+                            <div class="stars"></div>
+                        </div>
+                    </section>
+                </div>
+                <div class="last">
+                    <p>$${pay}</p>
+                    <button>DETAILS</button>
+                </div>
+            </article>
+            `
+        }
+    })
+}
+pushPopularJobs(jobs);
